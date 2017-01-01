adb0b95a6b5d6f72064537ddea7479c3ea5addee
philiphall1990 <philiphall9019@gmail.com>
2017-01-01 10:04:58 +0000

    style changes

--- GlobalVars.py
+++ GlobalVars.py
@@ -2,7 +2,6 @@
 
 '''This class holds global variables that persist and are used throughout the program'''
 class Vars:
-
     machine_code_instruction_list = list()
     machine_code_instruction_list_vectorised = list()
     machine_code_instruction_list_labels = list()
@@ -10,7 +9,6 @@ class Vars:
     source_code_while_statements_list = list()
     machine_code_operand_list = list()
     source_code_function_list = list()
-
     regular_expressions = {'machine_code_instruction' :
                                "([0-9A-Za-z]{1,5}):[\sa-zA-Z0-9]{2,}[\t|\s]([a-z]{3,6})\s*?([$%a-z0-9\(\)<>@+-]*?[,|\w])([$%a-z0-9\(\)<>@+-]*?)[#|\n]",
                            'if_statements' :
@@ -20,6 +18,4 @@ class Vars:
                            'machine code function calls' :
                                "([0-9a-f]{1,16}):\s *?([0-9a-zA-Z\s]{2,16}).*?<([a-zA-Z_]*).*?>[\t#|\n]",
                            'machine_code_function_decls' :
-                                "([a-zA-Z_]*?)\(\)"
-
-                           }
+                                "([a-zA-Z_]*?)\(\)"}
